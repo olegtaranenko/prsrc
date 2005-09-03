ad7823e56b93eccb5e6270211e48621aafe1357d
Pol <Pol@40be782e-4901-4cee-8daf-795e1e6e6383>
2005-09-03 19:57:16 +0000

    This commit was generated by cvs2svn to compensate for changes in r141, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn://www.markmaster.ru/trunk/prsrc@142 40be782e-4901-4cee-8daf-795e1e6e6383

---
+++ sql/transdata/drop_remote_prior.sql
@@ -0,0 +1,9 @@
+-- Удалить все remote таблицы
+-- Впредь будем использовать только процедуры!
+call build_remote_table('voc_names', 0);
+call build_remote_table('inv', 0);
+call build_remote_table('jmat', 0);
+call build_remote_table('mat', 0);
+call build_remote_table('jscet', 0);
+call build_remote_table('scet', 0);
+
