cb13b2eea02590e0f3cfbee817aadfb92de148da
Pol <Pol@40be782e-4901-4cee-8daf-795e1e6e6383>
2005-09-17 07:28:02 +0000

    This commit was generated by cvs2svn to compensate for changes in r161, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn://www.markmaster.ru/trunk/prsrc@162 40be782e-4901-4cee-8daf-795e1e6e6383

---
+++ sql/transdata/legacy_driver.sql
@@ -0,0 +1,52 @@
+begin
+declare icount int;
+declare folder_id integer;
+declare v_id_cur integer;
+
+select count(*) into icount from size;
+if icount <= 0 then
+	call legacy_guides();
+end if;
+
+
+--Производственные склады
+select id into folder_id from voc_names_stime where nm = 'Склады' and belong_id = 0;
+call move_old_voc_names(folder_id);
+call legacy_sklad();
+
+
+--Объекты затрат
+select id into folder_id from voc_names_stime where nm = 'Объекты затрат' and belong_id = 0;
+call move_old_voc_names(folder_id);
+call legacy_zatr();
+
+
+--Сторонние организации
+select id into folder_id from voc_names_stime where nm = 'Сторонние организации' and belong_id = 0;
+call move_old_voc_names(folder_id);
+call legacy_firms();
+
+-- Загрузить валюту "Условная единиа" и курс ее
+call legacy_currency();
+
+
+-- Номенклатура
+select id into folder_id from inv_stime where nm = 'Материалы' and belong_id = 0;
+call move_old_inv(folder_id);
+select id into folder_id from inv_stime where nm = 'Изделия' and belong_id = 0;
+call move_old_inv(folder_id);
+call legacy_inv();
+
+
+-- Заполнение вспомогательных таблиц для вариантных изделий
+call host_legacy_variant();
+
+-- Загрузить приходный ордера на склады в аналитическую базу stime
+call legacy_income_order();
+
+
+update system set trans_date = now();
+
+commit;
+
+end;
