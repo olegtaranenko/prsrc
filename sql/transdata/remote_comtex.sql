440b15c2cc1b88f04b8fcf8fc5a2f845ed3f25ce
Pol <Pol@40be782e-4901-4cee-8daf-795e1e6e6383>
2005-09-22 15:28:26 +0000

    This commit was generated by cvs2svn to compensate for changes in r193, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn://www.markmaster.ru/trunk/prsrc@194 40be782e-4901-4cee-8daf-795e1e6e6383

---
+++ sql/transdata/remote_comtex.sql
@@ -0,0 +1,42 @@
+if not exists (select 1 from sys.sysservers where srvname = 'prior') then  
+	create server prior class 'ASAODBC' USING 'DSN=prior;UID=dba;PWD=sql';
+end if;
+
+
+
+call build_host_procedure ( 
+		 'put_account'
+		, '  out out_exists integer'
+		+ ', inout p_sc char(26)'
+		+ ', inout p_sub char(10)'
+		+ ', inout p_name char(98)'
+		+ ', inout p_desc char(98)'
+);
+
+call build_host_procedure (
+		  'put_xoz', 
+
+		  '  p_server     char(50)'
+		+ ', p_id_xoz	  integer'
+		+ ', inout p_debit_sc   char(26)'
+		+ ', inout p_debit_sub  char(10)'
+		+ ', inout p_credit_sc  char(26)'
+		+ ', inout p_credit_sub char(10)'
+		+ ', p_dat        char(20)'
+		+ ', p_sum        real'
+		+ ', p_sumv       real'
+		+ ', p_id_curr    integer'
+		+ ', p_detail  char(99)'
+		+ ', p_purposeId  integer'
+);
+
+call build_host_procedure (
+		  'set_purpose', 
+		  '  p_purpose     char(99)'
+		+ ', inout p_debit_sc    char(26)'
+		+ ', inout p_debit_sub   char(10)'
+		+ ', inout p_credit_sc   char(26)'
+		+ ', inout p_credit_sub  char(10)'
+		+ ', out p_purposeId integer'
+);
+
