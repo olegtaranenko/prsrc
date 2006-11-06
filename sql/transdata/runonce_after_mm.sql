dc48477dfc0810b60c93f186cfd0c98584bc92a0
pm <pm@40be782e-4901-4cee-8daf-795e1e6e6383>
2006-11-06 13:02:28 +0000

    проблемы с накладными Rowmark (перепутаны id_jmat/id_mat)
    git-svn-id: svn://www.markmaster.ru/trunk/prsrc@502 40be782e-4901-4cee-8daf-795e1e6e6383

---
+++ sql/transdata/runonce_after_mm.sql
@@ -0,0 +1,9 @@
+delete from jmat where id in (16321, 16322, 16323);
+
+--update jmat set id = 16564 where id = 16321;
+
+--update jmat set id = 16565 where id = 16322;
+
+--update jmat set id = 16566 where id = 16323;
+
+commit;
---
+++ sql/transdata/runonce_after_prior.sql
@@ -0,0 +1,9 @@
+update sdocs set ventureid = 3 where numdoc = 6302406;
+update sdocs set ventureid = 3 where numdoc = 6302407;
+update sdocs set ventureid = 3 where numdoc = 6302408;
+
+update sdocs set ventureid = 2 where numdoc = 6302406;
+update sdocs set ventureid = 2 where numdoc = 6302407;
+update sdocs set ventureid = 2 where numdoc = 6302408;
+
+commit;
