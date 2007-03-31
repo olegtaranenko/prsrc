5ee29c85067036f5305687474abaa573c4a3eeed
pm <pm@40be782e-4901-4cee-8daf-795e1e6e6383>
2007-03-31 14:09:30 +0000

    Улучшения: Разделение процесса патча на ступени: ddl, code и dml В ком. строке можно указывать, какие ступени выполнять. (без параметра - только код) можно задавать лист, в котором явно указать file/server для исполнения.
    git-svn-id: svn://www.markmaster.ru/trunk/prsrc@642 40be782e-4901-4cee-8daf-795e1e6e6383

--- SP/update.bat
+++ SP/update.bat
@@ -1,8 +1,36 @@
 @echo off
+::echo on
+
+setlocal
+
 if (%1) == () goto usage
-
 set patch=%1
+shift
 
+set next_step=%1
+shift
+if (%next_step%) == () set next_step=code
+
+:loop
+if /i (%next_step%) == (ddl) goto start_ddl
+if /i (%next_step%) == (all) goto start_ddl
+goto code
+:start_ddl
+if (%ddl_executed%) == (1) goto code
+set ddl_executed=1
+echo Executing DDL...
+if not exist %patch%\ddl.lst set missed_file=ddl.lst&goto missed_warn
+for /F "tokens=*" %%i in (%patch%\ddl.lst) do call ../sql/transdata/isql.bat %patch%\%%i
+::for /F "tokens=*" %%i in (%patch%\sql.list) do echo call ../sql/transdata/isql.bat %patch%\%%i
+
+:code
+if /i (%next_step%) == (code) goto start_code
+if /i (%next_step%) == (all) goto start_code
+goto dml
+:start_code
+if (%code_executed%) == (1) goto dml
+echo Executing CODE...
+set code_executed=1
 if exist %patch%\logging_common.sql	call ../sql/transdata/isql.bat %patch%\logging_common.sql	prior
 if exist %patch%\logging_common.sql	call ../sql/transdata/isql.bat %patch%\logging_common.sql	stime
 if exist %patch%\logging_common.sql	call ../sql/transdata/isql.bat %patch%\logging_common.sql	pm
@@ -50,11 +78,45 @@ if exist %patch%\runonce_after_mm.sql	call ../sql/transdata/isql.bat %patch%\run
 
 if exist %patch%\runonce_after.bat	call %patch%\runonce_after.bat
 
-goto done
+
+:dml
+if /i (%next_step%) == (dml) goto start_dml
+if /i (%next_step%) == (all) goto start_dml
+goto list_file
+:start_dml
+if (%dml_executed%) == (1) goto list_file
+set dml_executed=1
+echo Executing DML...
+if not exist %patch%\dml.lst set missed_file=dml.lst&goto missed_warn
+for /F "tokens=*" %%i in (%patch%\dml.lst) do call ../sql/transdata/isql.bat %patch%\%%i
+::for /F "tokens=*" %%i in (%patch%\sql.list) do echo call ../sql/transdata/isql.bat %patch%\%%i
+
+:list_file
+::if not exist (%patch%\%next_step%.lst) goto next_done
+if /i (%next_step%) == (ddl) goto next_done
+if /i (%next_step%) == (code) goto next_done
+if /i (%next_step%) == (dml) goto next_done
+if /i (%next_step%) == (all) goto next_done
+echo Executing %next_step%.lst...
+for /F "tokens=*" %%i in (%patch%\%next_step%.lst) do call ../sql/transdata/isql.bat %patch%\%%i
+goto next_done
+
+:next_done
+set next_step=%1
+shift
+if (%next_step%) == () goto done
+goto loop
+
+:missed_warn
+if /i not (%next_step%) == (all) echo file %patch%\%missed_file% does not exists
+goto loop
 
 
 :usage
-echo Usage %~nx0  patch_name
+echo Usage %~nx0 patch_name {options}
+echo 	Opitons: ddl, code, dml, 
 
 :done
+
+endlocal
 
