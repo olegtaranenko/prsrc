1262527f450863fc6e1a6da272d6feb19fd68bef
otaranenko <otaranenko@40be782e-4901-4cee-8daf-795e1e6e6383>
2008-04-15 23:02:19 +0000

    Делинкуем только линкованные sql файлы
    git-svn-id: svn://www.markmaster.ru/trunk/prsrc@711 40be782e-4901-4cee-8daf-795e1e6e6383

--- SP/spdelink.bat
+++ SP/spdelink.bat
@@ -1,19 +1,20 @@
 @echo off
-
 if "%1" == "" goto usage
 
 
 set cur_filename=%~nx1
 set cur_file=%1
 set file_path=%~dp1
-echo %cur_file%
+set transdata_path=..\sql\transdata
 
 ::goto done
 ::echo on
 ::if exist %TEMP%\%cur_filename%.bak del %TEMP%\%cur_filename%.bak
+if not exist %transdata_path%\%cur_filename% goto done
+echo delinking %cur_filename%...
 copy /b /y %cur_file% %TEMP%\%cur_filename%.bak
 del %cur_file%
-copy /b /y ..\sql\transdata\%cur_filename% %cur_file% 
+copy /b /y %transdata_path%\%cur_filename% %cur_file% 
 goto done
 
 :panic
