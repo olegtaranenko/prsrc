6f36709c6ab4de59bce33e0097a7fcbd3fa41170
otaranenko <otaranenko@40be782e-4901-4cee-8daf-795e1e6e6383>
2008-05-26 18:10:32 +0000

    компилирование из командной строки
    git-svn-id: svn://www.markmaster.ru/trunk/prsrc@754 40be782e-4901-4cee-8daf-795e1e6e6383

---
+++ compile_all.bat
@@ -0,0 +1,10 @@
+cd Odll
+"C:\Program Files\Microsoft Visual Studio\VB98\VB6.EXE" /m Orders.vbp
+cd ..\Bay
+"C:\Program Files\Microsoft Visual Studio\VB98\VB6.EXE" /m Orders.vbp
+cd ..\Torge
+"C:\Program Files\Microsoft Visual Studio\VB98\VB6.EXE" /m Torge.vbp
+cd ..\cfg
+"C:\Program Files\Microsoft Visual Studio\VB98\VB6.EXE" /m cfg.vbp
+cd ..
+
