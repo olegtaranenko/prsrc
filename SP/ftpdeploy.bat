892cf4eac80db6606d70d5c0b4344e27bf1909d3
otaranenko <otaranenko@40be782e-4901-4cee-8daf-795e1e6e6383>
2008-05-26 18:22:38 +0000

    ftp deploy
    git-svn-id: svn://www.markmaster.ru/trunk/prsrc@755 40be782e-4901-4cee-8daf-795e1e6e6383

---
+++ SP/ftpdeploy.bat
@@ -0,0 +1,26 @@
+::@echo off
+if (%1) == () goto usage
+set patch=%1& shift
+
+:: delink junction files
+rar a %patch%\%patch% %patch%\*.* -r -x*.rar
+for %%i in ( %patch%\*.sql) do call spdelink %%i
+
+:: retrieve dump from the ftp-server
+echo open ftp.petmas.ru>ftpcmd.txt
+echo a>>ftpcmd.txt
+echo user admin RovWaig4>>ftpcmd.txt
+echo cd /pub/taranenko/sp>>ftpcmd.txt
+echo lcd %patch%>>ftpcmd.txt
+echo binary>>ftpcmd.txt
+echo send %patch%.rar>>ftpcmd.txt
+echo bye>>ftpcmd.txt
+ftp -s:ftpcmd.txt
+goto done
+
+:usage
+echo %~nx0 [patchNumber]
+echo WARNING: не использовать для ddl и dml файлы с расширением sql
+echo 	или которые не есть ЛИНК на соответствующий файл в директории transdata.
+goto done
+:done
