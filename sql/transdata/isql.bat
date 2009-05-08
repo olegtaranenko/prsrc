7959b4f9845d7e08cf7d0f72a1f21a80796c40a9
otaranenko <otaranenko@40be782e-4901-4cee-8daf-795e1e6e6383>
2009-05-08 10:30:57 +0000

    корректировки в transdata.
    git-svn-id: svn://www.markmaster.ru/trunk/prsrc@1051 40be782e-4901-4cee-8daf-795e1e6e6383

--- sql/transdata/isql.bat
+++ sql/transdata/isql.bat
@@ -22,32 +22,32 @@ if "%ALIAS_DSN%"=="" goto ext_dsn
 
 if /i not %ALIAS_DSN%==prior goto check_stime 
 set DSN=%DSN_PRIOR%
-if "%DSN%"=="" set DSN=DSN=%DEV_PREFIX%prior;UID=dba;PWD=sql 
+if "%DSN%"=="" set DSN=DSN=%DEV_PREFIX%prior;UID=dba;PWD=sql;
 goto check_isql_home
 
 :check_stime
 if /i not %ALIAS_DSN%==stime goto check_pm
 set DSN=%DSN_STIME%
-if "%DSN%"=="" set DSN=DSN=%DEV_PREFIX%stime;UID=admin;PWD=z 
+if "%DSN%"=="" set DSN=DSN=%DEV_PREFIX%stime;UID=admin;PWD=z;
 goto check_isql_home
 
 :check_pm
 if /i not %ALIAS_DSN%==pm goto check_mm 
 set DSN=%DSN_PM%
-if "%DSN%"=="" set DSN=DSN=%DEV_PREFIX%accountN;UID=admin;PWD=z 
+if "%DSN%"=="" set DSN=DSN=%DEV_PREFIX%accountN;UID=admin;PWD=z;
 goto check_isql_home
 
 :check_mm
 if /i not %ALIAS_DSN%==mm goto ext_dsn
 set DSN=%DSN_MM%
-if "%DSN%"=="" set DSN=DSN=%DEV_PREFIX%markmaster;UID=admin;PWD=z 
+if "%DSN%"=="" set DSN=DSN=%DEV_PREFIX%markmaster;UID=admin;PWD=z;
 goto check_isql_home
 
 :ext_dsn
 if "%DSN%"=="" goto badenvir
 :check_isql_home
 set ISQL_HOME=%asa8bin%
-if "%ISQL_HOME%"=="" set ISQL_HOME=C:\Program Files\Common Files\Comtec Shared\Sql8
+if "%ISQL_HOME%"=="" set ISQL_HOME=C:\Program Files\Sybase\SQL Anywhere 9\win32
 if "%ISQL_HOME%"=="" goto badenvir
 
 :exec
