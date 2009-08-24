27ef9cd01d4fd92f8f2122247521a68c791efd08
otaranenko <otaranenko@40be782e-4901-4cee-8daf-795e1e6e6383>
2009-08-24 17:04:48 +0000

    M94 nds separate files for server procedures
    git-svn-id: svn://www.markmaster.ru/trunk/prsrc@1176 40be782e-4901-4cee-8daf-795e1e6e6383

---
+++ dbupdate-parent/prior/src/main/liquibase/triggers/xPredmetyByIzdelia.wf_insert_izd.sql
@@ -0,0 +1,45 @@
+if exists (select 1 from systriggers where trigname = 'wf_insert_izd' and tname = 'xPredmetyByIzdelia') then 
+	drop trigger xPredmetyByIzdelia.wf_insert_izd;
+end if;
+
+create TRIGGER wf_insert_izd before insert on
+xPredmetyByIzdelia
+referencing new as new_name
+for each row
+begin
+	declare v_id_scet integer;
+	declare v_id_jscet integer;
+	declare v_id_inv integer;
+--	declare v_ventureid integer;
+	declare remoteServerNew varchar(32);
+	declare v_invCode varchar(10);
+	declare v_fields varchar(255);
+	declare v_values varchar(2000);
+	declare v_date date;
+	declare v_rate float;
+ 
+	select id_jscet, inDate, sysname, invCode, o.rate
+	into v_id_jscet, v_date, remoteServerNew, v_invcode, v_rate
+		from orders o
+		join GuideVenture v on o.ventureId = v.ventureId and v.standalone = 0
+		where numOrder = new_name.numOrder;
+
+	select id_inv into v_id_inv 
+		from sGuideProducts where prId = new_name.prId;
+  
+	if remoteServerNew is not null and v_id_jscet is not null then
+		set v_id_scet =	
+			wf_insert_scet (
+				remoteServerNew
+				, v_id_jscet
+				, v_id_inv
+				, new_name.quant
+				, new_name.cenaEd
+				, v_date
+				, v_rate
+			);
+		set new_name.id_scet = v_id_scet;
+		set new_name.id_inv = v_id_inv;
+	end if;
+end;
+
---
+++ dbupdate-parent/prior/src/main/liquibase/triggers/xPredmetyByIzdelia.wf_update_izd.sql
@@ -0,0 +1,38 @@
+if exists (select 1 from systriggers where trigname = 'wf_update_izd' and tname = 'xPredmetyByIzdelia') then 
+	drop trigger xPredmetyByIzdelia.wf_update_izd;
+end if;
+
+create TRIGGER wf_update_izd before update on
+xPredmetyByIzdelia
+referencing old as old_name new as new_name
+for each row
+begin
+	declare v_id_scet integer;
+	declare v_numorder integer;
+	declare v_belong_id integer;
+	declare remoteServerNew varchar(32);
+	declare v_values varchar(100);
+	declare v_fields varchar(200);
+	declare v_currency_rate float;
+	
+	set v_id_scet = old_name.id_scet;
+--	set v_numorder = old_name.numOrder;
+
+	select sysname, rate
+	into remoteServerNew, v_currency_rate
+		from orders o
+		join GuideVenture v on o.ventureId = v.ventureId and v.standalone = 0
+		where numOrder = old_name.numOrder;
+
+
+	if remoteServerNew is not null then
+		if update(quant) or update(cenaEd) then
+			call wf_scet_price_changed(remoteServerNew, new_name.quant, new_name.cenaEd, v_id_scet, v_currency_rate)
+		end if;
+		if update(quant) then
+			call update_remote(remoteServerNew, 'scet', 'kol1', convert(varchar(20), new_name.quant), 'id = ' + convert(varchar(20), v_id_scet));
+		end if;
+	end if;
+  
+end;
+
