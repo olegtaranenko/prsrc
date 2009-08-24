8a4331fc6df044acf0397d8268f3cb2a4929bc32
otaranenko <otaranenko@40be782e-4901-4cee-8daf-795e1e6e6383>
2009-08-24 19:53:12 +0000

    move proc to separate file
    git-svn-id: svn://www.markmaster.ru/trunk/prsrc@1179 40be782e-4901-4cee-8daf-795e1e6e6383

---
+++ dbupdate-parent/prior/src/main/liquibase/procedures/wf_set_invoice_detail.sql
@@ -0,0 +1,117 @@
+if exists (select '*' from sysprocedure where proc_name like 'wf_set_invoice_detail') then  
+	drop procedure wf_set_invoice_detail;
+end if;
+
+
+create procedure wf_set_invoice_detail (
+			p_servername varchar(20)
+			, p_id_jscet integer
+			, p_numOrder integer
+			, p_date date
+			, p_rate float
+)
+begin
+-- Процедура синхронизирует предметы заказа Приора
+-- с предметами счета в бухгалтерской базе комтеха
+-- Это нужно сделать, если в заказ сначала 
+-- добавть предметы, а только потом назначить предприятие,
+-- через которую этот заказ должен пройти.
+
+	declare v_id_scet integer;
+	declare v_id_inv integer;
+	declare is_variant integer;
+	declare v_id_variant integer;
+	declare is_uslug integer;
+	declare v_quant float;
+	declare v_perList float;
+
+	set is_uslug = 1; // предполагаем изначально, что да
+
+
+	for c_nomenk as n dynamic scroll cursor for
+		select 
+			  p.nomNom as r_nomNom
+			, p.quant as r_quant
+			, p.cenaEd as r_cenaEd
+		from xPredmetybynomenk p
+		where p.numOrder = p_numOrder
+	do
+	    set is_uslug = 0; -- есть предметы к заказу, значит не услуга
+
+		select id_inv, perList into v_id_inv, v_perList from sGuideNomenk where nomnom = r_nomNom;
+		
+		set v_id_scet = 
+			wf_insert_scet(
+				p_servername
+				, p_id_jscet
+				, v_id_inv
+				, r_quant / v_perList
+				, r_cenaEd * v_perList
+				, p_date
+				, p_rate
+			);
+		update xPredmetyByNomenk set id_scet = v_id_scet where current of n;
+
+	end for;
+
+
+	for c_izd as i dynamic scroll cursor for
+		select 
+			  prId as r_prId
+			, prExt as r_prExt
+			, quant as r_quant
+			, cenaEd as r_cenaEd
+		from xPredmetyByIzdelia p
+		where p.numOrder = p_numOrder
+	do
+
+	    set is_uslug = 0; -- есть предметы к заказу, значит не услуга
+		select id_inv into v_id_inv from sGuideProducts where prId = r_prId;
+
+		-- смотрим, является ли изделие вариантным?
+		
+		select count(*) into is_variant from sVariantPower where productId = r_prId;
+		if is_variant = 1 then
+			-- ищем и/или добавляем вариант в Inv
+			set v_id_variant = wf_get_variant_id(p_numOrder, r_prId, r_prExt);
+			select id_inv into v_id_inv 
+			from sGuideComplect 
+			where 
+				id_variant = v_id_variant;
+		end if;
+
+		set v_id_scet = 
+			wf_insert_scet(
+				p_servername
+				, p_id_jscet
+				, v_id_inv
+				, r_quant
+				, r_cenaEd
+				, p_date
+				, p_rate
+			);
+
+		update xPredmetyByIzdelia set id_scet = v_id_scet, id_inv = v_id_inv where current of i;
+	end for;  -- цикла по изделиям
+
+	select ordered into v_quant from orders where numorder = p_numOrder;
+	if is_uslug = 1 and abs(v_quant) > 0.001 then
+		-- ищем товар под названием "услуга"
+		select id_inv into v_id_inv from sGuideNomenk where nomNom = 'УСЛ';
+
+
+		set v_id_scet = 
+			wf_insert_scet(
+				p_servername
+				, p_id_jscet
+				, v_id_inv
+				, 1 // quant
+				, v_quant//r_cenaEd
+				, now()//p_date
+				, p_rate
+			);
+
+	end if;
+
+
+end;
