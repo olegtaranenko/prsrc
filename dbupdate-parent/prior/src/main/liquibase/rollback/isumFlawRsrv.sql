1419bc86e7696dd135f9fe20359ab86c4e97f0b7
otaranenko <otaranenko@40be782e-4901-4cee-8daf-795e1e6e6383>
2010-06-17 07:11:29 +0000

    Простой вынос вьюх из файла transdata/rollback в одиночные файлы
    git-svn-id: svn://www.markmaster.ru/trunk/prsrc@1449 40be782e-4901-4cee-8daf-795e1e6e6383

---
+++ dbupdate-parent/prior/src/main/liquibase/rollback/isumFlawRsrv.sql
@@ -0,0 +1,10 @@
+if exists (select 1 from sysviews where viewname = 'isumFlawRsrv' and vcreator = 'dba') then
+	drop view isumFlawRsrv;
+end if;
+
+create view isumFlawRsrv (numorder, nomnom, quant, status, date1, manager, client, note, ceh, sm_zakazano, sm_paid)
+as
+select
+r.numorder, r.nomnom, r.quant, null, r.date1, null, 'Списание', null, r.note, null, null
+from itemFlawRsrv r
+;
--- dbupdate-parent/prior/src/main/liquibase/rollback/itemFlawRsrv.sql
+++ dbupdate-parent/prior/src/main/liquibase/rollback/itemFlawRsrv.sql
@@ -10,13 +10,3 @@ join sdmcrez r on r.numdoc = d.numdoc
 where d.numext = 0
 ;
 
-if exists (select 1 from sysviews where viewname = 'isumFlawRsrv' and vcreator = 'dba') then
-	drop view isumFlawRsrv;
-end if;
-
-create view isumFlawRsrv (numorder, nomnom, quant, status, date1, manager, client, note, ceh, sm_zakazano, sm_paid)
-as
-select
-r.numorder, r.nomnom, r.quant, null, r.date1, null, 'Списание', null, r.note, null, null
-from itemFlawRsrv r
-;
