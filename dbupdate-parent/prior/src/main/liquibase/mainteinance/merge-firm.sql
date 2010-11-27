82147b4b2d11297fe2af8b05b9cab83ee30a316c
otaranenko <otaranenko@40be782e-4901-4cee-8daf-795e1e6e6383>
2010-11-27 09:13:13 +0000

    sync with production
    git-svn-id: svn://www.markmaster.ru/trunk/prsrc@1704 40be782e-4901-4cee-8daf-795e1e6e6383

--- dbupdate-parent/prior/src/main/liquibase/mainteinance/merge-firm.sql
+++ dbupdate-parent/prior/src/main/liquibase/mainteinance/merge-firm.sql
@@ -1,3 +1,3 @@
-	update ${orders.table.name} set firmid = ${main.firm.id} where firmid = ${deleted.firm.id};
-
-	delete from ${firms.table.name} where firmid = ${deleted.firm.id};
+update ${orders.table.name} set firmid = ${main.firm.id} where firmid = ${deleted.firm.id};
+delete from ${firms.table.name} where firmid = ${deleted.firm.id};
+commit;
